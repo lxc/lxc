0886644ac2ded657eb4e00dca879d16cd4365a73
dlezcano <dlezcano>
2008-09-03 15:43:18 +0000

    fix little format

--- src/liblxc/lxc_log.h
+++ src/liblxc/lxc_log.h
@@ -12,7 +12,7 @@
 #define lxc_log_debug(format, ...) lxc_log(format, "debug", ##__VA_ARGS__);
 #define lxc_log_trace(format, ...) lxc_log(format, "trace", ##__VA_ARGS__);
 #define lxc_log_syserror(format, ...) do { \
-		fprintf(stderr, "[SYSERROR][%s] \t%s:%d - " format "\n", \
+		fprintf(stderr, "[syserr][%s] \t%s:%d - " format "\n", \
 			strerror(errno),__FUNCTION__, __LINE__, \
 			##__VA_ARGS__);					\
 	} while (0)
