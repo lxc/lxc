732db53f602755094a8e8dafb6c9c113db7ca0cb
dlezcano <dlezcano>
2008-10-28 22:06:58 +0000

    Added a helper to generate the Changelog between two releases

--- release.sh
+++ release.sh
@@ -11,11 +11,24 @@ MINOR=$3
 MICRO=$4
 VERSION=$MAJOR\_$MINOR\_$MICRO
 TAG=$NAME\_$VERSION
-echo
-echo -n "Ready to tag $TAG [Y/n] "
-read
 
-test -z "$REPLY" || echo $REPLY | egrep "y|Y|Yes" && \
-    cvs tag $TAG
-test ! -z "$REPLY" && echo "Aborted"
-exit 0
+release_tag() {
+    echo
+    echo -n "Ready to tag $TAG [Y/n] "
+    read
+
+    test -z "$REPLY" || echo $REPLY | egrep "y|Y|Yes" && \
+	cvs tag $TAG
+    test ! -z "$REPLY" && echo "Aborted"
+}
+
+release_changelog() {
+    echo "Generating ChangeLog"
+    echo -n "Please give the previous tag name: "
+    read 
+    cvs2cl --delta $REPLY:$TAG --stdout
+}
+
+release_tag
+
+release_changelog
