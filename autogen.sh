ccf1319832ffeb8e07fcef9bfe67e96e283fead7
Daniel Lezcano <daniel.lezcano@free.fr>
2009-04-26 11:43:41 +0200

    add aclocal include in makefile
    When auto-regen is done at the make time, eg. when the configure.in
    script has been modified, the configure is not correctly generated
    because the 'config' directory is missing for aclocal, and that makes
    the generation to fail due to the missing macros defined in config
    directory.
    
    Signed-off-by: Daniel Lezcano <dlezcano@fr.ibm.com>

--- autogen.sh
+++ autogen.sh
@@ -4,9 +4,8 @@ set -x
 
 test -d autom4te.cache && rm -rf autom4te.cache
 test -d m4 || mkdir m4
-ACLOCAL_AMFLAGS="-I m4 -I config $ACLOCAL_AMFLAGS"
 libtoolize --force --copy
-aclocal $ACLOCAL_AMFLAGS || exit 1
+aclocal -I m4 -I config || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake --add-missing --copy || exit 1
