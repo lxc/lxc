8db2cb8e0d1ee9e6edb0b5e9bd158c8789a5e5b6
Daniel Lezcano <daniel.lezcano@free.fr>
2009-04-26 11:43:41 +0200

    remove m4 dir usage
    As the m4 directory is not used, remove the usage from
    the scripts.
    
    Signed-off-by: Daniel Lezcano <dlezcano@fr.ibm.com>

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,8 @@
 set -x
 
 test -d autom4te.cache && rm -rf autom4te.cache
-test -d m4 || mkdir m4
 libtoolize --force --copy
-aclocal -I m4 -I config || exit 1
+aclocal -I config || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake --add-missing --copy || exit 1
