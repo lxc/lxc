5b46e19c3f0c3f89e8d853d0239b5ab3a2e049a2
Cedric Le Goater <legoater@free.fr>
2009-10-22 15:33:40 +0200

    lxc: kill libtool
    This is useless in a Linux only environment. The .so version is
    the version of the package.
    
    Signed-off-by: Cedric Le Goater <clg@fr.ibm.com>
    Signed-off-by: Daniel Lezcano <dlezcano@fr.ibm.com>

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,7 @@
 set -x
 
 test -d autom4te.cache && rm -rf autom4te.cache
-libtoolize --force --copy
-aclocal -I config || exit 1
+ aclocal -I config || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake --add-missing --copy || exit 1
