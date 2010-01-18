5fa5aa7c6af5ccce78873c06e37ddaa6efd9dd17
Clement Calmels <clement.calmels@fr.ibm.com>
2010-01-18 23:08:12 +0100

    remove warning in parse.c:80
    Signed-off-by: Clement Calmels <clement.calmels@fr.ibm.com>
    Signed-off-by: Daniel Lezcano <dlezcano@fr.ibm.com>

--- src/lxc/parse.c
+++ src/lxc/parse.c
@@ -20,8 +20,10 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-#include <string.h>
+#define _GNU_SOURCE
 #include <stdio.h>
+#undef _GNU_SOURCE
+#include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <dirent.h>
