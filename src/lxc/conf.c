f62b344996937459ae5f31b0358cb440ddde421f
Serge Hallyn <serge.hallyn@ubuntu.com>
2012-11-13 21:35:51 -0600

    dont fail on failure to link kmsg
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -2290,10 +2290,8 @@ int lxc_setup(const char *name, struct lxc_conf *lxc_conf)
 		return -1;
 	}
 
-	if (setup_kmsg(&lxc_conf->rootfs, &lxc_conf->console)) {
+	if (setup_kmsg(&lxc_conf->rootfs, &lxc_conf->console))  // don't fail
 		ERROR("failed to setup kmsg for '%s'", name);
-		return -1;
-	}
 
 	if (setup_tty(&lxc_conf->rootfs, &lxc_conf->tty_info, lxc_conf->ttydir)) {
 		ERROR("failed to setup the ttys for '%s'", name);
