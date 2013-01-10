f298ee50b8cfa46f20138f95ca916ec4b54c9a45
Stéphane Graber <stgraber@ubuntu.com>
2013-01-10 17:10:51 -0500

    utmp.h: Don't fail when utmpx.h isn't present
    Following a comment on the mailing-list, I made utmp.h return -1
    when it's disabled, the problem with that is that it prevents the
    container from starting completely, which isn't quite what I wanted.
    
    This change makes the function succeed, the container will therefore
    start but without utmp handler.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>
    Acked-by: Serge E. Hallyn <serge.hallyn@ubuntu.com>

--- src/lxc/utmp.h
+++ src/lxc/utmp.h
@@ -32,6 +32,6 @@ int lxc_utmp_mainloop_add(struct lxc_epoll_descr *descr,
 #else
 static inline int lxc_utmp_mainloop_add(struct lxc_epoll_descr *descr,
 			  struct lxc_handler *handler) {
-		return -1;
+		return 0;
 }
 #endif
