50266dc604ac8df63328e84376414de7448869b1
Dwight Engen <dwight.engen@oracle.com>
2013-10-29 14:49:16 -0400

    coverity 1097618: check for NULL return from calloc before deref
    Signed-off-by: Dwight Engen <dwight.engen@oracle.com>
    Acked-by: Stéphane Graber <stgraber@ubuntu.com>

--- src/lxc/cgroup.c
+++ src/lxc/cgroup.c
@@ -1688,6 +1688,8 @@ char *cgroup_to_absolute_path(struct cgroup_mount_point *mp, const char *path, c
 
 	len = strlen(mp->mount_point) + strlen(path) + (suffix ? strlen(suffix) : 0);
 	buf = calloc(len + 1, 1);
+	if (!buf)
+		return NULL;
 	rv = snprintf(buf, len + 1, "%s%s%s", mp->mount_point, path, suffix ? suffix : "");
 	if (rv > len) {
 		free(buf);
