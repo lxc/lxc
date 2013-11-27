cf642e1085063cc0081335169ffd83d7cab3899c
Stéphane Graber <stgraber@ubuntu.com>
2013-11-27 17:59:35 -0500

    Fix typo in doxygen comment
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- src/lxc/lxccontainer.h
+++ src/lxc/lxccontainer.h
@@ -510,7 +510,7 @@ struct lxc_container {
 	 *  By default the original is used (subject to snapshot requirements).
 	 * \param bdevdata Information about how to create the new storage
 	 *  (i.e. fstype and fsdata).
-	 *  \param newsize In case of a block device backing store, an
+	 * \param newsize In case of a block device backing store, an
 	 *  optional size. If \c 0, the original backing store's size will
 	 *  be used if possible. Note this only applies to the rootfs. For
 	 *  any other filesystems, the original size will be duplicated.
