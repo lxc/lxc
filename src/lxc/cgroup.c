28bb9321e2c55aa93097487e41c3aca95e0c106c
Qiang Huang <h.huangqiang@huawei.com>
2014-01-15 12:09:26 +0800

    cgroup.c: redefine the valid cgroup name
    Signed-off-by: Qiang Huang <h.huangqiang@huawei.com>
    Acked-by: Stéphane Graber <stgraber@ubuntu.com>

--- src/lxc/cgroup.c
+++ src/lxc/cgroup.c
@@ -1669,7 +1669,11 @@ bool is_valid_cgroup(const char *name)
 {
 	const char *p;
 	for (p = name; *p; p++) {
-		if (*p < 32 || *p == 127 || *p == '/')
+		/* Use the ASCII printable characters range(32 - 127)
+		 * is reasonable, we kick out 32(SPACE) because it'll
+		 * break legacy lxc-ls
+		 */
+		if (*p <= 32 || *p >= 127 || *p == '/')
 			return false;
 	}
 	return strcmp(name, ".") != 0 && strcmp(name, "..") != 0;
