92e23841a270bc48fc4a63fcac833e6d112486eb
Stéphane Graber <stgraber@ubuntu.com>
2014-01-24 13:27:26 -0500

    Support __NR_set_ns as well.
    Apparently some older systems didn't have __NR_setns but had __NR_set_ns.
    
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- src/lxc/utils.h
+++ src/lxc/utils.h
@@ -56,6 +56,8 @@ static inline int setns(int fd, int nstype)
 {
 #ifdef __NR_setns
 	return syscall(__NR_setns, fd, nstype);
+#elif __NR_set_ns
+	return syscall(__NR_set_ns, fd, nstype);
 #else
 	errno = ENOSYS;
 	return -1;
