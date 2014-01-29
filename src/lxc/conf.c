565e571c8f88aeef8c23bf79479b0fde3c8e069b
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-01-29 15:17:06 +0000

    userns_exec_1: catch errors in the spawned process.
    lxc_map_ids can call system(3), which on error from the
    spawned process returns > 0.  No path should return > 0
    when it meant success.  So check the lxc_map_ids() value
    to be != rather than just < 0.
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>
    Acked-by: Stéphane Graber <stgraber@ubuntu.com>

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -4055,7 +4055,7 @@ int userns_exec_1(struct lxc_conf *conf, int (*fn)(void *), void *data)
 	ret = lxc_map_ids(idmap, pid);
 	lxc_free_idmap(idmap);
 	free(idmap);
-	if (ret < 0) {
+	if (ret) {
 		ERROR("Error setting up child mappings");
 		goto err;
 	}
