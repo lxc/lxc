e4399ab9ac6946ce734845f1083775368197349c
Stéphane Graber <stgraber@ubuntu.com>
2014-02-07 19:36:50 -0500

    Whitespace fix
    Signed-off-by: Stéphane Graber <stgraber@ubuntu.com>

--- autogen.sh
+++ autogen.sh
@@ -24,7 +24,7 @@
 set -x
 
 test -d autom4te.cache && rm -rf autom4te.cache
- aclocal -I config || exit 1
+aclocal -I config || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake --add-missing --copy || exit 1
