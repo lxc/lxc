7ceebfd12a0503bee5eaab8d4c062a4389939a61
Dwight Engen <dwight.engen@oracle.com>
2014-03-27 16:46:38 -0400

    add yum plugin to repatch rootfs on yum update
    oracle-template: Split patching rootfs vs one time setup into separate
    shell functions so the template can be run with --patch.
    
    oracle-template: Update to install the yum plugin and itself (as lxc-patch)
    into a container. The plugin just runs lxc-patch --patch <path> so it is
    fairly generic, but in this case it is running a copy of the template inside
    the container.
    
    Signed-off-by: Dwight Engen <dwight.engen@oracle.com>
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

---
+++ config/yum/lxc-patch.py
@@ -0,0 +1,56 @@
+# Yum plugin to re-patch container rootfs after a yum update is done
+#
+# Copyright (C) 2012 Oracle
+#
+# Authors:
+# Dwight Engen <dwight.engen@oracle.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+import os
+from fnmatch import fnmatch
+from yum.plugins import TYPE_INTERACTIVE
+from yum.plugins import PluginYumExit
+
+requires_api_version = '2.0'
+plugin_type = (TYPE_INTERACTIVE,)
+
+def posttrans_hook(conduit):
+    pkgs = []
+    patch_required = False
+
+    # If we aren't root, we can't have updated anything
+    if os.geteuid():
+        return
+
+    # See what packages have files that were patched
+    confpkgs = conduit.confString('main', 'packages')
+    if not confpkgs:
+        return
+
+    tmp = confpkgs.split(",")
+    for confpkg in tmp:
+        pkgs.append(confpkg.strip())
+
+    conduit.info(2, "lxc-patch: checking if updated pkgs need patching...")
+    ts = conduit.getTsInfo()
+    for tsmem in ts.getMembers():
+        for pkg in pkgs:
+            if fnmatch(pkg, tsmem.po.name):
+                patch_required = True
+    if patch_required:
+        conduit.info(2, "lxc-patch: patching container...")
+        os.spawnlp(os.P_WAIT, "lxc-patch", "lxc-patch", "--patch", "/")
