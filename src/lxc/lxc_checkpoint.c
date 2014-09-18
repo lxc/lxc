b56d64e0c5eb9b053fa30761c3ced6e36fb828af
Tycho Andersen <tycho.andersen@canonical.com>
2014-09-18 09:43:08 -0500

    lxc-checkpoint should actually log things
    Looks like lxc-checkpoint was missing the log inititalization code, so it never
    actually logged anything when the options were provided.
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>
    Acked-by: Stéphane Graber <stgraber@ubuntu.com>

--- src/lxc/lxc_checkpoint.c
+++ src/lxc/lxc_checkpoint.c
@@ -174,6 +174,15 @@ int main(int argc, char *argv[])
 	if (lxc_arguments_parse(&my_args, argc, argv))
 		exit(1);
 
+	if (!my_args.log_file)
+		my_args.log_file = "none";
+
+	if (lxc_log_init(my_args.name, my_args.log_file, my_args.log_priority,
+			 my_args.progname, my_args.quiet, my_args.lxcpath[0]))
+		exit(1);
+
+	lxc_log_options_no_override();
+
 	c = lxc_container_new(my_args.name, my_args.lxcpath[0]);
 	if (!c) {
 		fprintf(stderr, "System error loading %s\n", my_args.name);
