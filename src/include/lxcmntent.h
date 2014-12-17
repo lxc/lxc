15df3138eb06676863c93e6c6bd7473dcb3919f6
Patrick O'Leary <patrick.oleary@gmail.com>
2014-12-16 19:52:00 -0600

    lxcmntent: remove most bionic compatability
    Newer Android platforms have a version of bionic libc which supports
    most of these functions.
    
    Signed-off-by: Patrick O'Leary <patrick.oleary@gmail.com>

--- src/include/lxcmntent.h
+++ src/include/lxcmntent.h
@@ -22,32 +22,12 @@
 #define _lxcmntent_h
 
 #include <../config.h>
-
+#include <mntent.h>
 #if IS_BIONIC
-struct mntent
-{
-    char* mnt_fsname;
-    char* mnt_dir;
-    char* mnt_type;
-    char* mnt_opts;
-    int mnt_freq;
-    int mnt_passno;
-};
-
-extern struct mntent *getmntent (FILE *stream);
-extern struct mntent *getmntent_r (FILE *stream, struct mntent *mp, char *buffer, int bufsiz);
-#endif
-
-#ifndef HAVE_SETMNTENT
-FILE *setmntent (const char *file, const char *mode);
-#endif
-
-#ifndef HAVE_ENDMNTENT
-int endmntent (FILE *stream);
-#endif
 
 #ifndef HAVE_HASMNTOPT
 extern char *hasmntopt (const struct mntent *mnt, const char *opt);
 #endif
 
+#endif
 #endif
