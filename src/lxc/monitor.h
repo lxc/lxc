2366b8a769b9403fda3c4780c84ad87d07b21d7e
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-04-21 13:34:45 -0500

    use poll instead of select
    Particularly when using the go-lxc api with lots of threads, it
    happens that if the open files limit is > 1024, we will try to
    select on fd > 1024 which breaks on glibc.
    
    So use poll instead of select.
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>
    Acked-by: Stéphane Graber <stgraber@ubuntu.com>

--- src/lxc/monitor.h
+++ src/lxc/monitor.h
@@ -26,6 +26,7 @@
 #include <limits.h>
 #include <sys/param.h>
 #include <sys/un.h>
+#include <poll.h>
 
 #include "conf.h"
 
@@ -41,7 +42,6 @@ struct lxc_msg {
 	int value;
 };
 
-extern int lxc_monitor_open(const char *lxcpath);
 extern int lxc_monitor_sock_name(const char *lxcpath, struct sockaddr_un *addr);
 extern int lxc_monitor_fifo_name(const char *lxcpath, char *fifo_path,
 				 size_t fifo_path_sz, int do_mkdirp);
@@ -50,5 +50,45 @@ extern void lxc_monitor_send_state(const char *name, lxc_state_t state,
 extern void lxc_monitor_send_exit_code(const char *name, int exit_code,
 			    const char *lxcpath);
 extern int lxc_monitord_spawn(const char *lxcpath);
+
+/*
+ * Open the monitoring mechanism for a specific container
+ * The function will return an fd corresponding to the events
+ * Returns a file descriptor on success, < 0 otherwise
+ */
+extern int lxc_monitor_open(const char *lxcpath);
+
+/*
+ * Blocking read for the next container state change
+ * @fd  : the file descriptor provided by lxc_monitor_open
+ * @msg : the variable which will be filled with the state
+ * Returns 0 if the monitored container has exited, > 0 if
+ * data was read, < 0 otherwise
+ */
+extern int lxc_monitor_read(int fd, struct lxc_msg *msg);
+
+/*
+ * Blocking read for the next container state change with timeout
+ * @fd      : the file descriptor provided by lxc_monitor_open
+ * @msg     : the variable which will be filled with the state
+ * @timeout : the timeout in seconds to wait for a state change
+ * Returns 0 if the monitored container has exited, > 0 if
+ * data was read, < 0 otherwise
+ */
+extern int lxc_monitor_read_timeout(int fd, struct lxc_msg *msg, int timeout);
+
+/*
+ * Blocking read from multiple monitors for the next container state
+ * change with timeout
+ * @fds     : struct pollfd descripting the fds to use
+ * @nfds    : the number of entries in fds
+ * @msg     : the variable which will be filled with the state
+ * @timeout : the timeout in seconds to wait for a state change
+ * Returns 0 if the monitored container has exited, > 0 if
+ * data was read, < 0 otherwise
+ */
+extern int lxc_monitor_read_fdset(struct pollfd *fds, nfds_t nfds, struct lxc_msg *msg,
+			   int timeout);
+
 
 #endif
