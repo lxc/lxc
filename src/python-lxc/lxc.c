d51f78ed31f88c0d69def88eec38d76cef685b9e
Aleksandr Mezin <mezin.alexander@gmail.com>
2016-05-26 16:03:57 +0600

    python-lxc: don't use private lxc/namespace.h
    Signed-off-by: Aleksandr Mezin <mezin.alexander@gmail.com>

--- src/python-lxc/lxc.c
+++ src/python-lxc/lxc.c
@@ -26,10 +26,38 @@
 #include "structmember.h"
 #include <lxc/lxccontainer.h>
 #include "lxc/utils.h"
-#include "lxc/namespace.h"
 #include "lxc/confile.h"
 #include <stdio.h>
 #include <sys/wait.h>
+#include <sched.h>
+
+/*
+ * CLONE_* definitions copied from lxc/namespace.h
+ */
+#ifndef CLONE_FS
+#  define CLONE_FS                0x00000200
+#endif
+#ifndef CLONE_NEWNS
+#  define CLONE_NEWNS             0x00020000
+#endif
+#ifndef CLONE_NEWCGROUP
+#  define CLONE_NEWCGROUP         0x02000000
+#endif
+#ifndef CLONE_NEWUTS
+#  define CLONE_NEWUTS            0x04000000
+#endif
+#ifndef CLONE_NEWIPC
+#  define CLONE_NEWIPC            0x08000000
+#endif
+#ifndef CLONE_NEWUSER
+#  define CLONE_NEWUSER           0x10000000
+#endif
+#ifndef CLONE_NEWPID
+#  define CLONE_NEWPID            0x20000000
+#endif
+#ifndef CLONE_NEWNET
+#  define CLONE_NEWNET            0x40000000
+#endif
 
 /* Helper functions */
 
