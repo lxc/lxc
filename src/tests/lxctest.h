03d3bbc95dfc882e03764c1d7a06427de6ea40b9
Christian Brauner <cbrauner@suse.de>
2016-08-13 22:38:52 +0200

    tests: add unit tests for lxc_string_replace()
    Signed-off-by: Christian Brauner <cbrauner@suse.de>

---
+++ src/tests/lxctest.h
@@ -0,0 +1,42 @@
+/*
+ * lxc: linux Container library
+ *
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * Authors:
+ * Christian Brauner <christian.brauner@mailbox.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef __LXC_TEST_H_
+#define __LXC_TEST_H_
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define lxc_test_assert_stringify(expression, stringify_expression)            \
+	do {                                                                   \
+		if (!(expression)) {                                           \
+			fprintf(stderr, "%s: %s: %d: %s\n", __FILE__,          \
+				__func__, __LINE__, stringify_expression);     \
+			abort();                                               \
+		}                                                              \
+	} while (false)
+
+#define lxc_test_assert_abort(expression) lxc_test_assert_stringify(expression, #expression)
+
+#endif /* __LXC_TEST_H */
