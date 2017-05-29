c3760156b844b4a5dd0ca48610a8f1e7a4ef6621
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-29 14:35:13 +0200

    confile: config_network_ipv4()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -882,7 +882,7 @@ static int config_network_ipv4(const char *key, const char *value,
 	struct lxc_list *list;
 	char *cursor, *slash, *addr = NULL, *bcast = NULL, *prefix = NULL;
 
-	if (!value || !strlen(value))
+	if (config_value_empty(value))
 		return lxc_clear_config_item(lxc_conf, key);
 
 	netdev = network_netdev(key, value, &lxc_conf->network);
