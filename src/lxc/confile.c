4100d1a7865276ad584cd32bcdc246aaff5707bc
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-30 04:50:09 +0200

    confile: config_stopsignal()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -1621,9 +1621,13 @@ static int config_stopsignal(const char *key, const char *value,
 {
 	int sig_n;
 
-	if (config_value_empty(value))
+	/* Set config value to default. */
+	if (config_value_empty(value)) {
+		lxc_conf->stopsignal = 0;
 		return 0;
+	}
 
+	/* Parse new config value. */
 	sig_n = sig_parse(value);
 	if (sig_n < 0)
 		return -1;
