b84663a8c5d55bb0e73bd54712934a244de88bbc
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-30 13:29:46 +0200

    utils: add lxc_safe_ulong()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/utils.c
+++ src/lxc/utils.c
@@ -2021,6 +2021,29 @@ int lxc_safe_uint(const char *numstr, unsigned int *converted)
 	return 0;
 }
 
+int lxc_safe_ulong(const char *numstr, unsigned long *converted)
+{
+	char *err = NULL;
+	unsigned long int uli;
+
+	while (isspace(*numstr))
+		numstr++;
+
+	if (*numstr == '-')
+		return -EINVAL;
+
+	errno = 0;
+	uli = strtoul(numstr, &err, 0);
+	if (errno == ERANGE && uli == ULONG_MAX)
+		return -ERANGE;
+
+	if (err == numstr || *err != '\0')
+		return -EINVAL;
+
+	*converted = uli;
+	return 0;
+}
+
 int lxc_safe_int(const char *numstr, int *converted)
 {
 	char *err = NULL;
