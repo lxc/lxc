4d294a0d51f927dc035d0c9f8892edd9fd609349
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-30 17:38:49 +0200

    confile: allow to retrieve lxc.rebootsignal
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -2861,6 +2861,8 @@ int lxc_get_config_item(struct lxc_conf *c, const char *key, char *retv,
 		return lxc_get_idmaps(c, retv, inlen);
 	else if (strcmp(key, "lxc.haltsignal") == 0)
 		return lxc_get_conf_int(c, retv, inlen, c->haltsignal);
+	else if (strcmp(key, "lxc.rebootsignal") == 0)
+		return lxc_get_conf_int(c, retv, inlen, c->rebootsignal);
 	else return -1;
 
 	if (!v)
