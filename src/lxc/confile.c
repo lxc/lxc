0728e39544eaf8fd469d13f3cbcd79ac6f88d88d
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-31 15:19:23 +0200

    confile: add clearer for lxc.mount.auto
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -108,6 +108,7 @@ static int clr_config_mount(const char *, struct lxc_conf *);
 
 static int set_config_mount_auto(const char *, const char *, struct lxc_conf *);
 static int get_config_mount_auto(const char *, char *, int, struct lxc_conf *);
+static int clr_config_mount_auto(const char *, struct lxc_conf *);
 
 static int set_config_fstab(const char *, const char *, struct lxc_conf *);
 static int get_config_fstab(const char *, char *, int, struct lxc_conf *);
@@ -221,7 +222,7 @@ static struct lxc_config_t config[] = {
 	{ "lxc.loglevel",             set_config_loglevel,             get_config_loglevel,          clr_config_loglevel,          },
 	{ "lxc.logfile",              set_config_logfile,              get_config_logfile,           clr_config_logfile,           },
 	{ "lxc.mount.entry",          set_config_mount,                get_config_mount,             clr_config_mount,             },
-	{ "lxc.mount.auto",           set_config_mount_auto,           get_config_mount_auto,        NULL },
+	{ "lxc.mount.auto",           set_config_mount_auto,           get_config_mount_auto,        clr_config_mount_auto, },
 	{ "lxc.mount",                set_config_fstab,	               get_config_fstab,             NULL },
 	{ "lxc.rootfs.mount",         set_config_rootfs_mount,         get_config_rootfs_mount,      NULL },
 	{ "lxc.rootfs.options",       set_config_rootfs_options,       get_config_rootfs_options,    NULL },
@@ -2551,9 +2552,6 @@ int lxc_clear_config_item(struct lxc_conf *c, const char *key)
 	} else if (strcmp(key, "lxc.cap.keep") == 0) {
 		ret = lxc_clear_config_keepcaps(c);
 
-	} else if (strcmp(key, "lxc.mount.auto") == 0) {
-		ret = lxc_clear_automounts(c);
-
 	} else if (strncmp(key, "lxc.hook", 8) == 0) {
 		ret = lxc_clear_hooks(c, key);
 
@@ -3783,4 +3781,9 @@ static inline int clr_config_logfile(const char *key, struct lxc_conf *c)
 static inline int clr_config_mount(const char *key, struct lxc_conf *c)
 {
 	return lxc_clear_mount_entries(c);
+}
+
+static inline int clr_config_mount_auto(const char *key, struct lxc_conf *c)
+{
+	return lxc_clear_automounts(c);
 }
