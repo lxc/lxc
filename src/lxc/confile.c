33ef5490cd4070367f2e22709d5951404ad16e76
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-31 07:41:23 +0200

    confile: add getter for lxc.rootfs.backend
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -109,6 +109,8 @@ static int set_config_rootfs_options(const char *, const char *, struct lxc_conf
 static int get_config_rootfs_options(struct lxc_container *, const char *, char *, int);
 
 static int set_config_rootfs_backend(const char *, const char *, struct lxc_conf *);
+static int get_config_rootfs_backend(struct lxc_container *, const char *, char *, int);
+
 static int set_config_pivotdir(const char *, const char *, struct lxc_conf *);
 static int set_config_utsname(const char *, const char *, struct lxc_conf *);
 static int set_config_hook(const char *, const char *, struct lxc_conf *lxc_conf);
@@ -166,7 +168,7 @@ static struct lxc_config_t config[] = {
 	{ "lxc.mount",                set_config_fstab,	               get_config_fstab,             NULL},
 	{ "lxc.rootfs.mount",         set_config_rootfs_mount,         get_config_rootfs_mount,      NULL},
 	{ "lxc.rootfs.options",       set_config_rootfs_options,       get_config_rootfs_options,    NULL},
-	{ "lxc.rootfs.backend",       set_config_rootfs_backend,        NULL, NULL},
+	{ "lxc.rootfs.backend",       set_config_rootfs_backend,       get_config_rootfs_backend,    NULL},
 	{ "lxc.rootfs",               set_config_rootfs,               get_config_rootfs,            NULL},
 	{ "lxc.pivotdir",             set_config_pivotdir,              NULL, NULL},
 	{ "lxc.utsname",              set_config_utsname,               NULL, NULL},
@@ -2671,8 +2673,6 @@ int lxc_get_config_item(struct lxc_conf *c, const char *key, char *retv,
 		v = c->console.log_path;
 	else if (strcmp(key, "lxc.console") == 0)
 		v = c->console.path;
-	else if (strcmp(key, "lxc.rootfs.backend") == 0)
-		v = c->rootfs.bdev_type;
 	else if (strcmp(key, "lxc.cap.drop") == 0)
 		return lxc_get_item_cap_drop(c, retv, inlen);
 	else if (strcmp(key, "lxc.cap.keep") == 0)
@@ -3543,4 +3543,10 @@ static int get_config_rootfs_options(struct lxc_container *c, const char *key,
 				     char *retv, int inlen)
 {
 	return lxc_get_conf_str(retv, inlen, c->lxc_conf->rootfs.options);
+}
+
+static int get_config_rootfs_backend(struct lxc_container *c, const char *key,
+				     char *retv, int inlen)
+{
+	return lxc_get_conf_str(retv, inlen, c->lxc_conf->rootfs.bdev_type);
 }
