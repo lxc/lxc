51eb1491a570d72a6f7779a62a738abe0e015268
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-31 18:25:51 +0200

    confile: add clearer for lxc.environment
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -217,6 +217,7 @@ static int clr_config_group(const char *, struct lxc_conf *);
 
 static int set_config_environment(const char *, const char *, struct lxc_conf *);
 static int get_config_environment(const char *, char *, int, struct lxc_conf *);
+static int clr_config_environment(const char *, struct lxc_conf *);
 
 static int set_config_init_cmd(const char *, const char *, struct lxc_conf *);
 static int get_config_init_cmd(const char *, char *, int, struct lxc_conf *);
@@ -294,7 +295,7 @@ static struct lxc_config_t config[] = {
 	{ "lxc.start.order",          set_config_start,                get_config_start,             clr_config_start,             },
 	{ "lxc.monitor.unshare",      set_config_monitor,              get_config_monitor,           clr_config_monitor,           },
 	{ "lxc.group",                set_config_group,                get_config_group,             clr_config_group,             },
-	{ "lxc.environment",          set_config_environment,          get_config_environment,       NULL },
+	{ "lxc.environment",          set_config_environment,          get_config_environment,       clr_config_environment,       },
 	{ "lxc.init_cmd",             set_config_init_cmd,             get_config_init_cmd,          NULL },
 	{ "lxc.init_uid",             set_config_init_uid,             get_config_init_uid,          NULL },
 	{ "lxc.init_gid",             set_config_init_gid,             get_config_init_gid,          NULL },
@@ -2562,10 +2563,7 @@ int lxc_clear_config_item(struct lxc_conf *c, const char *key)
 {
 	int ret = 0;
 
-	if (strncmp(key, "lxc.environment", 15) == 0) {
-		ret = lxc_clear_environment(c);
-
-	} else if (strcmp(key, "lxc.init_cmd") == 0) {
+	if (strcmp(key, "lxc.init_cmd") == 0) {
 		free(c->init_cmd);
 		c->init_cmd = NULL;
 
@@ -3881,4 +3879,8 @@ static inline int clr_config_group(const char *key, struct lxc_conf *c)
 {
 	return lxc_clear_groups(c);
 }
+
+static inline int clr_config_environment(const char *key, struct lxc_conf *c)
+{
+	return lxc_clear_environment(c);
 }
