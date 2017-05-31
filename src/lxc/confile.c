079eff026687dca47c4d1dbb0ffd87fa73890f2c
Christian Brauner <christian.brauner@ubuntu.com>
2017-05-31 15:12:18 +0200

    confile: add clearer for lxc.id_map
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -92,6 +92,7 @@ static int clr_config_cgroup(const char *, struct lxc_conf *);
 
 static int set_config_idmaps(const char *, const char *, struct lxc_conf *);
 static int get_config_idmaps(const char *, char *, int, struct lxc_conf *);
+static int clr_config_idmaps(const char *, struct lxc_conf *);
 
 static int set_config_loglevel(const char *, const char *, struct lxc_conf *);
 static int get_config_loglevel(const char *, char *, int, struct lxc_conf *);
@@ -213,7 +214,7 @@ static struct lxc_config_t config[] = {
 	{ "lxc.aa_allow_incomplete",  set_config_lsm_aa_incomplete,    get_config_lsm_aa_incomplete, clr_config_lsm_aa_incomplete, },
 	{ "lxc.se_context",           set_config_lsm_se_context,       get_config_lsm_se_context,    clr_config_lsm_se_context,    },
 	{ "lxc.cgroup",               set_config_cgroup,               get_config_cgroup,            clr_config_cgroup,            },
-	{ "lxc.id_map",               set_config_idmaps,               get_config_idmaps,            NULL },
+	{ "lxc.id_map",               set_config_idmaps,               get_config_idmaps,            clr_config_idmaps,            },
 	{ "lxc.loglevel",             set_config_loglevel,             get_config_loglevel,          NULL },
 	{ "lxc.logfile",              set_config_logfile,              get_config_logfile,           NULL },
 	{ "lxc.mount.entry",          set_config_mount,                get_config_mount,             NULL },
@@ -2562,9 +2563,6 @@ int lxc_clear_config_item(struct lxc_conf *c, const char *key)
 	} else if (strncmp(key, "lxc.environment", 15) == 0) {
 		ret = lxc_clear_environment(c);
 
-	} else if (strncmp(key, "lxc.id_map", 10) == 0) {
-		ret = lxc_clear_idmaps(c);
-
 	} else if (strcmp(key, "lxc.utsname") == 0) {
 		free(c->utsname);
 		c->utsname = NULL;
@@ -3769,4 +3767,9 @@ static inline int clr_config_lsm_se_context(const char *key, struct lxc_conf *c)
 static inline int clr_config_cgroup(const char *key, struct lxc_conf *c)
 {
 	return lxc_clear_cgroups(c, key);
+}
+
+static inline int clr_config_idmaps(const char *key, struct lxc_conf *c)
+{
+	return lxc_clear_idmaps(c);
 }
