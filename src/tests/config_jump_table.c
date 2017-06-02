c04f651ededff1d55081770686f57918763abb82
Christian Brauner <christian.brauner@ubuntu.com>
2017-06-02 02:42:42 +0200

    tests: enforce all methods for config items
    This adds a test that checks LXC's configuration jump table whether all methods
    for a given configuration item are implemented. If it is not, we'll error out.
    This should provide additional safety since a) the API can now be sure that
    dereferencing the pointer for a given method in the config struct is safe and
    b) when users implement new configuration keys and forget to implement a
    required method we'll see it right away.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

---
+++ src/tests/config_jump_table.c
@@ -0,0 +1,94 @@
+/* liblxcapi
+ *
+ * Copyright © 2017 Christian Brauner <christian.brauner@ubuntu.com>.
+ * Copyright © 2017 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+#include <lxc/lxccontainer.h>
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include "confile.h"
+#include "lxc/state.h"
+#include "lxctest.h"
+
+int main(int argc, char *argv[])
+{
+	int fulllen = 0, inlen = 0, ret = EXIT_FAILURE;
+	char *key, *keys, *saveptr = NULL;
+
+	fulllen = lxc_listconfigs(NULL, inlen);
+
+	keys = malloc(sizeof(char) * fulllen + 1);
+	if (!keys) {
+		lxc_error("%s\n", "failed to allocate memory");
+		exit(ret);
+	}
+
+	if (lxc_listconfigs(keys, fulllen) != fulllen) {
+		lxc_error("%s\n", "failed to retrieve configuration keys");
+		goto on_error;
+	}
+
+	for (key = strtok_r(keys, "\n", &saveptr); key != NULL;
+	     key = strtok_r(NULL, "\n", &saveptr)) {
+		struct lxc_config_t *config;
+		config = lxc_getconfig(key);
+		if (!config) {
+			lxc_error("configuration key \"%s\" not implemented in "
+				  "jump table",
+				  key);
+			goto on_error;
+		}
+
+		if (!config->set) {
+			lxc_error("configuration key \"%s\" has no set method "
+				  "in jump table",
+				  key);
+			goto on_error;
+		}
+
+		if (!config->get) {
+			lxc_error("configuration key \"%s\" has no get method "
+				  "in jump table",
+				  key);
+			goto on_error;
+		}
+
+		if (!config->clr) {
+			lxc_error("configuration key \"%s\" has no clr (clear) "
+				  "method in jump table",
+				  key);
+			goto on_error;
+		}
+	}
+
+	ret = EXIT_SUCCESS;
+
+on_error:
+	free(keys);
+
+	exit(ret);
+
+}
