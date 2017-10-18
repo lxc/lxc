127c6e703b0a10b68aef44e8951b0507d72efd90
Christian Brauner <christian.brauner@ubuntu.com>
2017-10-18 19:48:57 +0200

    utils: add lxc_make_tmpfile()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/utils.c
+++ src/lxc/utils.c
@@ -2339,3 +2339,23 @@ bool lxc_nic_exists(char *nic)
 
 	return true;
 }
+
+int lxc_make_tmpfile(char *template, bool rm)
+{
+	int fd, ret;
+
+	fd = mkstemp(template);
+	if (fd < 0)
+		return -1;
+
+	if (!rm)
+		return fd;
+
+	ret = unlink(template);
+	if (ret < 0) {
+		close(fd);
+		return -1;
+	}
+
+	return fd;
+}
