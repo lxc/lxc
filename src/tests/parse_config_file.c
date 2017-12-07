7edd05402cb54d9a7220e4390426054a86811348
LiFeng <lifeng68@huawei.com>
2017-12-07 11:40:35 -0500

    confile: Add lxc.sysctl config
    Signed-off-by: LiFeng <lifeng68@huawei.com>

--- src/tests/parse_config_file.c
+++ src/tests/parse_config_file.c
@@ -922,6 +922,13 @@ int main(int argc, char *argv[])
 		goto non_test_error;
 	}
 
+	/* lxc.sysctl */
+	if (set_get_compare_clear_save_load(c, "lxc.sysctl.net.core.somaxconn", "256", tmpf,
+					    true) < 0) {
+		lxc_error("%s\n", "lxc.sysctl.net.core.somaxconn");
+		goto non_test_error;
+	}
+
 	/* REMOVE IN LXC 3.0
 	   legacy lxc.limit.* key
 	 */
