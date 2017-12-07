f4e1fa60b27cea4ec558d0a9b8cffebc99dd8d5d
Christian Brauner <christian.brauner@ubuntu.com>
2017-12-07 15:14:37 +0100

    coverity: #1425921
    free allocated memory
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/attach.c
+++ src/lxc/attach.c
@@ -599,6 +599,7 @@ static char *lxc_attach_getpwshell(uid_t uid)
 		if (waitpid(pid, &status, 0) < 0) {
 			if (errno == EINTR)
 				goto again;
+			free(result);
 			return NULL;
 		}
 
@@ -607,14 +608,20 @@ static char *lxc_attach_getpwshell(uid_t uid)
 		 * don't.
 		 */
 
-		if (!WIFEXITED(status))
+		if (!WIFEXITED(status)) {
+			free(result);
 			return NULL;
+		}
 
-		if (WEXITSTATUS(status) != 0)
+		if (WEXITSTATUS(status) != 0) {
+			free(result);
 			return NULL;
+		}
 
-		if (!found)
+		if (!found) {
+			free(result);
 			return NULL;
+		}
 
 		return result;
 	} else {
