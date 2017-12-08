6dd366633d9ff88cf8dabbd303ba80868c8c70e0
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2017-12-08 00:02:18 -0200

    lxc: Distinguish pthread_mutex_unlock error messages
    The same message exists in lxclock.c and cgmanager.c, so print the
    filename along with the message.
    
    Before this patch:
    lxc-destroy -n u1
    pthread_mutex_unlock returned:1 Operation not permitted
    
    After this patch:
    xc-destroy -n u1
    lxclock.c: pthread_mutex_unlock returned:1 Operation not permitted
    
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- src/lxc/cgroups/cgmanager.c
+++ src/lxc/cgroups/cgmanager.c
@@ -88,7 +88,8 @@ static void unlock_mutex(pthread_mutex_t *l)
 	int ret;
 
 	if ((ret = pthread_mutex_unlock(l)) != 0) {
-		fprintf(stderr, "pthread_mutex_unlock returned:%d %s\n", ret, strerror(ret));
+		fprintf(stderr, "%s: pthread_mutex_unlock returned:%d %s\n",
+				__FILE__, ret, strerror(ret));
 		exit(1);
 	}
 }
