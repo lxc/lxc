3c3e0cb5309c6eddc3004284e096074650f14395
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2017-12-30 16:35:52 -0200

    lxc_config: Add -h and --help flags handler
    As the other tools already handle, show usage message when -h or --help
    are used.
    
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- src/lxc/lxc_config.c
+++ src/lxc/lxc_config.c
@@ -59,7 +59,8 @@ int main(int argc, char *argv[])
 {
 	struct lxc_config_items *i;
 
-	if (argc < 2)
+	if (argc < 2 || strcmp(argv[1], "-h") == 0 ||
+			strcmp(argv[1], "--help") == 0)
 		usage(argv[0]);
 	if (strcmp(argv[1], "-l") == 0)
 		list_config_items();
