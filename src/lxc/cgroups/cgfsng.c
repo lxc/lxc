438c4581b068db39e88db570294a852ecc31092f
Christian Brauner <christian.brauner@ubuntu.com>
2018-02-17 16:32:16 +0100

    cgfsng: order includes
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/cgroups/cgfsng.c
+++ src/lxc/cgroups/cgfsng.c
@@ -44,10 +44,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-
-#include <linux/types.h>
 #include <linux/kdev_t.h>
+#include <linux/types.h>
+#include <sys/types.h>
 
 #include "caps.h"
 #include "cgroup.h"
