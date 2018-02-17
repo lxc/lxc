3fd0de4d307aa9146c272a556c3b4cd10d5f1936
Christian Brauner <christian.brauner@ubuntu.com>
2018-02-17 17:08:06 +0100

    cgfsng: add me to authors
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/cgroups/cgfsng.c
+++ src/lxc/cgroups/cgfsng.c
@@ -5,6 +5,7 @@
  *
  * Authors:
  * Serge Hallyn <serge.hallyn@ubuntu.com>
+ * Christian Brauner <christian.brauner@ubuntu.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
