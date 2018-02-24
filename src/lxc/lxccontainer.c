76cee3b4302d948c23efe22c21d308759d293ecb
Christian Brauner <christian.brauner@ubuntu.com>
2018-02-24 15:21:16 +0100

    lxccontainer: do_lxcapi_want_daemonize()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/lxccontainer.c
+++ src/lxc/lxccontainer.c
@@ -611,12 +611,13 @@ static bool do_lxcapi_want_daemonize(struct lxc_container *c, bool state)
 {
 	if (!c || !c->lxc_conf)
 		return false;
-	if (container_mem_lock(c)) {
-		ERROR("Error getting mem lock");
+
+	if (container_mem_lock(c))
 		return false;
-	}
+
 	c->daemonize = state;
 	container_mem_unlock(c);
+
 	return true;
 }
 
