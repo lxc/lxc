aef02a14c94b92ff201f211bad9621f7c3e2fa4c
Simos Xenitellis <simos.lists@googlemail.com>
2018-05-15 01:47:16 +0300

    Fixed resource leak in userns_exec_full() at conf.c
    coverity: #1425836
    
    Signed-off-by: Simos Xenitellis <simos.lists@googlemail.com>

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -4082,8 +4082,10 @@ struct lxc_list *get_minimal_idmap(struct lxc_conf *conf)
 	return idmap;
 
 on_error:
-	if (idmap)
+	if (idmap) {
 		lxc_free_idmap(idmap);
+		free(id_map);
+	}
 	if (container_root_uid)
 		free(container_root_uid);
 	if (container_root_gid)
