48ae7fbaa8afc5a8a2f72f546936f1db61b1a393
Simos Xenitellis <simos.lists@googlemail.com>
2018-05-15 01:39:27 +0300

    Fixed resource leak in suggest_default_idmap() at conf.c
    coverity: #1425802
    coverity: #1425844
    
    Signed-off-by: Simos Xenitellis <simos.lists@googlemail.com>

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -4497,6 +4497,8 @@ void suggest_default_idmap(void)
 	if (!urange || !grange) {
 		ERROR("You do not have subuids or subgids allocated");
 		ERROR("Unprivileged containers require subuids and subgids");
+		free(gname);
+		free(uname);
 		return;
 	}
 
