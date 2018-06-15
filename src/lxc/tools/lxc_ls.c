cdba253fe248594af82b77a4233411a1521e8c6e
Christian Brauner <christian.brauner@ubuntu.com>
2018-06-15 11:49:10 +0200

    coverity: #1425813
    Resource leak
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/tools/lxc_ls.c
+++ src/lxc/tools/lxc_ls.c
@@ -568,8 +568,10 @@ static int ls_get(struct ls **m, size_t *size, const struct lxc_arguments *args,
 			/* We want to remove all locks we create under
 			 * /run/lxc/lock so we create a string pointing us to
 			 * the lock path for the current container. */
-			if (ls_remove_lock(path, name, lockpath, &len_lockpath, true) == -1)
+			if (ls_remove_lock(path, name, lockpath, &len_lockpath, true) == -1) {
+				free(newpath);
 				goto put_and_next;
+			}
 
 			ls_get(m, size, args, newpath, l->name, lvl + 1, lockpath, len_lockpath, grps_must, grps_must_len);
 			free(newpath);
