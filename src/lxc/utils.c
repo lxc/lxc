77fe0837b2ebe6fd9e8204ae25a000fbd8b342e6
2xsec <dh48.jeong@samsung.com>
2018-06-29 15:23:43 +0900

    tools: lxc-device: share internal API symbols
    Signed-off-by: 2xsec <dh48.jeong@samsung.com>

--- src/lxc/utils.c
+++ src/lxc/utils.c
@@ -1273,13 +1273,13 @@ bool switch_to_ns(pid_t pid, const char *ns) {
 
 	fd = open(nspath, O_RDONLY);
 	if (fd < 0) {
-		SYSERROR("failed to open %s", nspath);
+		SYSERROR("Failed to open %s", nspath);
 		return false;
 	}
 
 	ret = setns(fd, 0);
 	if (ret) {
-		SYSERROR("failed to set process %d to %s of %d.", pid, ns, fd);
+		SYSERROR("Failed to set process %d to %s of %d.", pid, ns, fd);
 		close(fd);
 		return false;
 	}
