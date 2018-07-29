979f9e34c36be01ca916faf805248cfeae8957df
Christian Brauner <christian.brauner@ubuntu.com>
2018-07-29 23:02:14 +0200

    conf: s/pipe()/pipe2()/g
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -4359,7 +4359,7 @@ int userns_exec_1(struct lxc_conf *conf, int (*fn)(void *), void *data,
 	if (!idmap)
 		return -1;
 
-	ret = pipe(p);
+	ret = pipe2(p, O_CLOEXEC);
 	if (ret < 0) {
 		SYSERROR("Failed to create pipe");
 		return -1;
@@ -4441,7 +4441,7 @@ int userns_exec_full(struct lxc_conf *conf, int (*fn)(void *), void *data,
 	if (!conf)
 		return -EINVAL;
 
-	ret = pipe(p);
+	ret = pipe2(p, O_CLOEXEC);
 	if (ret < 0) {
 		SYSERROR("opening pipe");
 		return -1;
