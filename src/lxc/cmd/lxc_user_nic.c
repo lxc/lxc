75ca3dc6aa526c9b24dacb6107c7b1b43e756771
Tobin C. Harding <me@tobin.cc>
2018-08-17 16:13:29 +1000

    cmd: Put trailing */ on a separate line
    checkpatch emits warning:
    
        WARNING: Block comments use a trailing */ on a separate line
    
    Put trailing */ on a separate line.
    
    Signed-off-by: Tobin C. Harding <me@tobin.cc>

--- src/lxc/cmd/lxc_user_nic.c
+++ src/lxc/cmd/lxc_user_nic.c
@@ -512,7 +512,8 @@ static int instantiate_veth(char *veth1, char *veth2)
 
 	/* Changing the high byte of the mac address to 0xfe, the bridge
 	 * interface will always keep the host's mac address and not take the
-	 * mac address of a container. */
+	 * mac address of a container.
+	 */
 	ret = setup_private_host_hw_addr(veth1);
 	if (ret < 0)
 		usernic_error("Failed to change mac address of host interface "
