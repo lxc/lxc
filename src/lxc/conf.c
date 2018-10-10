2de12765e2286ca59099fd22b27460ccfe575682
Christian Brauner <christian.brauner@ubuntu.com>
2018-10-10 13:53:00 +0200

    conf: log prlimit setup
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -2647,8 +2647,10 @@ int setup_resource_limits(struct lxc_list *limits, pid_t pid)
 			SYSERROR("Failed to set limit %s", lim->resource);
 			return -1;
 		}
+
+		TRACE("Setup \"%s\" limit", lim->resource);
 #else
-		ERROR("Cannot set limit %s as prlimit is missing", lim->resource);
+		ERROR("Cannot set limit \"%s\" as prlimit is missing", lim->resource);
 		return -1;
 #endif
 	}
