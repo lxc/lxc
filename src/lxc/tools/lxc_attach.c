0c816b346788afa9d601766e31544fdcce67d780
Christian Brauner <christian.brauner@ubuntu.com>
2019-02-13 23:08:11 +0100

    rexec: remove needless /proc/cmdline parsing
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/tools/lxc_attach.c
+++ src/lxc/tools/lxc_attach.c
@@ -57,9 +57,9 @@ lxc_log_define(lxc_attach, lxc);
  * mapping: CVE-2019-5736.
  */
 #ifdef ENFORCE_MEMFD_REXEC
-__attribute__((constructor)) static void lxc_attach_rexec(void)
+__attribute__((constructor)) static void lxc_attach_rexec(int argc, char *argv[])
 {
-	if (!getenv("LXC_MEMFD_REXEC") && lxc_rexec("lxc-attach")) {
+	if (!getenv("LXC_MEMFD_REXEC") && lxc_rexec(argv, "lxc-attach")) {
 		fprintf(stderr, "Failed to re-execute lxc-attach via memory file descriptor\n");
 		_exit(EXIT_FAILURE);
 	}
