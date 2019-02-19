652342782f5e9ac782a0bad6a395c5d8df99c0bf
Christian Brauner <christian.brauner@ubuntu.com>
2019-02-19 23:44:08 +0100

    conf: cleanup macros parse_propagationopts
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/conf.c
+++ src/lxc/conf.c
@@ -1912,7 +1912,8 @@ static void parse_propagationopt(char *opt, unsigned long *flags)
 
 int parse_propagationopts(const char *mntopts, unsigned long *pflags)
 {
-	char *p, *s;
+	__do_free char *s = NULL;
+	char *p;
 
 	if (!mntopts)
 		return 0;
@@ -1926,7 +1927,6 @@ int parse_propagationopts(const char *mntopts, unsigned long *pflags)
 	*pflags = 0L;
 	lxc_iterate_parts(p, s, ",")
 		parse_propagationopt(p, pflags);
-	free(s);
 
 	return 0;
 }
