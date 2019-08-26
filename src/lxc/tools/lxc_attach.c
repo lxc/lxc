lxc/lxc

commit ae68cad763d5b39a6a9e51de2acd1ad128b720ca

lxc-attach: switch to attach_run_wait
Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

src/lxc/tools/lxc_attach.c
@@ -354,19 +354,23 @@ int main(int argc, char *argv[])
 			goto out;
 	}
 
-	if (command.program)
-		ret = c->attach(c, lxc_attach_run_command, &command, &attach_options, &pid);
-	else
+	if (command.program) {
+		ret = c->attach_run_wait(c, &attach_options, command.program,
+					 (const char **)command.argv);
+		if (ret < 0)
+			goto out;
+	} else {
 		ret = c->attach(c, lxc_attach_run_shell, NULL, &attach_options, &pid);
-	if (ret < 0)
-		goto out;
+		if (ret < 0)
+			goto out;
 
-	ret = lxc_wait_for_pid_status(pid);
-	if (ret < 0)
-		goto out;
+		ret = lxc_wait_for_pid_status(pid);
+		if (ret < 0)
+			goto out;
 
-	if (WIFEXITED(ret))
-		wexit = WEXITSTATUS(ret);
+		if (WIFEXITED(ret))
+			wexit = WEXITSTATUS(ret);
+	}
 
 out:
 	lxc_container_put(c);

commit 576b950f6c3b1f7d8d32d9e5c3e6ec053e393765

lxc-attach: make sure exit status of command is returned
Commit ae68cad763d5b39a6a9e51de2acd1ad128b720ca introduced a regression that
makes lxc-attach ignore the exit status of the executed command. This was first
identified in 3.0.4 LTS, while it worked on 3.0.3.

  # lxc-attach --version
  3.0.4
  # lxc-attach -n test false; echo $?
  0

Signed-off-by: Antonio Terceiro <terceiro@debian.org>
Bug-Debian: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=934983

src/lxc/tools/lxc_attach.c
@@ -385,10 +385,9 @@ int main(int argc, char *argv[])
 		ret = lxc_wait_for_pid_status(pid);
 		if (ret < 0)
 			goto out;
-
-		if (WIFEXITED(ret))
-			wexit = WEXITSTATUS(ret);
 	}
+	if (WIFEXITED(ret))
+		wexit = WEXITSTATUS(ret);
 
 out:
 	lxc_container_put(c);