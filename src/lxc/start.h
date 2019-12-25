b94283e19f19495d6d853d43146f2c640542c751
Christian Brauner <christian.brauner@ubuntu.com>
2019-12-25 18:50:53 +0100

    start: remove procfs pidfd support
    We'll only rely on proper anon-inode based pidfd support in the future.
    There's no good reason to use the procfs fallback. All the fancy features we
    might want to use are only available with anon-inode pidfds.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/start.h
+++ src/lxc/start.h
@@ -83,12 +83,6 @@ struct lxc_handler {
 	/* The child's pidfd. */
 	int pidfd;
 
-	/*
-	 * File descriptor for the /proc/<pid> directory of the container's
-	 * init process.
-	 */
-	int proc_pidfd;
-
 	/* The grandfather's pid when double-forking. */
 	pid_t transient_pid;
 
