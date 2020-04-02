c353b0b9508988d4aeb44fa3d0403d801a63f2a8
Christian Brauner <christian.brauner@ubuntu.com>
2020-04-02 11:50:27 +0200

    utils: rework fix_stdio_permissions()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/attach.c
+++ src/lxc/attach.c
@@ -875,9 +875,11 @@ static int attach_child_main(struct attach_clone_payload *payload)
 
 	if (new_gid == ns_root_gid)
 		new_gid = LXC_INVALID_GID;
-	
+
 	/* Make sure that the processes STDIO is correctly owned by the user that we are switching to */
-	fix_stdio_permissions(new_uid);
+	ret = fix_stdio_permissions(new_uid);
+	if (ret)
+		WARN("Failed to ajust stdio permissions");
 
 	if (!lxc_switch_uid_gid(new_uid, new_gid))
 		goto on_error;
