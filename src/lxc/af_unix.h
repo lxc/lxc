84a00796325dd639d3c43808e75f39666c2e07ef
Christian Brauner <christian.brauner@ubuntu.com>
2020-07-17 23:14:38 +0200

    af_unix: hide unnecessary symbols
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/af_unix.h
+++ src/lxc/af_unix.h
@@ -10,35 +10,31 @@
 #include "compiler.h"
 
 /* does not enforce \0-termination */
-extern int lxc_abstract_unix_open(const char *path, int type, int flags);
-extern void lxc_abstract_unix_close(int fd);
+__hidden extern int lxc_abstract_unix_open(const char *path, int type, int flags);
+__hidden extern void lxc_abstract_unix_close(int fd);
 /* does not enforce \0-termination */
-extern int lxc_abstract_unix_connect(const char *path);
+__hidden extern int lxc_abstract_unix_connect(const char *path);
 
-extern int lxc_abstract_unix_send_fds(int fd, int *sendfds, int num_sendfds,
-				      void *data, size_t size)
-__access_r(2, 3) __access_r(4, 5);
+__hidden extern int lxc_abstract_unix_send_fds(int fd, int *sendfds, int num_sendfds, void *data,
+					       size_t size) __access_r(2, 3) __access_r(4, 5);
 
-extern int lxc_abstract_unix_send_fds_iov(int fd, int *sendfds, int num_sendfds,
-					  struct iovec *iov, size_t iovlen)
-__access_r(2, 3);
+__hidden extern int lxc_abstract_unix_send_fds_iov(int fd, int *sendfds, int num_sendfds,
+						   struct iovec *iov, size_t iovlen) __access_r(2, 3);
 
-extern int lxc_abstract_unix_recv_fds(int fd, int *recvfds, int num_recvfds,
-				      void *data, size_t size)
-__access_r(2, 3) __access_r(4, 5);
+__hidden extern int lxc_abstract_unix_recv_fds(int fd, int *recvfds, int num_recvfds, void *data,
+					       size_t size) __access_r(2, 3) __access_r(4, 5);
 
-extern int lxc_unix_send_fds(int fd, int *sendfds, int num_sendfds, void *data,
-			     size_t size);
+__hidden extern int lxc_unix_send_fds(int fd, int *sendfds, int num_sendfds, void *data, size_t size);
 
-extern int lxc_abstract_unix_send_credential(int fd, void *data, size_t size)
-__access_r(2, 3);
+__hidden extern int lxc_abstract_unix_send_credential(int fd, void *data, size_t size)
+    __access_r(2, 3);
 
-extern int lxc_abstract_unix_rcv_credential(int fd, void *data, size_t size)
-__access_w(2, 3);
+__hidden extern int lxc_abstract_unix_rcv_credential(int fd, void *data, size_t size)
+    __access_w(2, 3);
 
-extern int lxc_unix_sockaddr(struct sockaddr_un *ret, const char *path);
-extern int lxc_unix_connect(struct sockaddr_un *addr);
-extern int lxc_unix_connect_type(struct sockaddr_un *addr, int type);
-extern int lxc_socket_set_timeout(int fd, int rcv_timeout, int snd_timeout);
+__hidden extern int lxc_unix_sockaddr(struct sockaddr_un *ret, const char *path);
+__hidden extern int lxc_unix_connect(struct sockaddr_un *addr);
+__hidden extern int lxc_unix_connect_type(struct sockaddr_un *addr, int type);
+__hidden extern int lxc_socket_set_timeout(int fd, int rcv_timeout, int snd_timeout);
 
 #endif /* __LXC_AF_UNIX_H */
