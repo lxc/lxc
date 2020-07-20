590b39e50bbd8fff52efad3b39e0edf359121301
Christian Brauner <christian.brauner@ubuntu.com>
2020-07-20 16:11:14 +0200

    criu: hide unnecessary symbols
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/criu.h
+++ src/lxc/criu.h
@@ -7,9 +7,9 @@
 
 #include <lxc/lxccontainer.h>
 
-extern bool __criu_pre_dump(struct lxc_container *c, struct migrate_opts *opts);
-extern bool __criu_dump(struct lxc_container *c, struct migrate_opts *opts);
-extern bool __criu_restore(struct lxc_container *c, struct migrate_opts *opts);
-extern bool __criu_check_feature(uint64_t *features_to_check);
+__hidden extern bool __criu_pre_dump(struct lxc_container *c, struct migrate_opts *opts);
+__hidden extern bool __criu_dump(struct lxc_container *c, struct migrate_opts *opts);
+__hidden extern bool __criu_restore(struct lxc_container *c, struct migrate_opts *opts);
+__hidden extern bool __criu_check_feature(uint64_t *features_to_check);
 
 #endif
