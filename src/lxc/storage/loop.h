64143d355a69c7ed827479fd3d1c53a94d76eda6
Christian Brauner <christian.brauner@ubuntu.com>
2020-07-21 13:57:16 +0200

    storage: hide unnecessary symbols
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/storage/loop.h
+++ src/lxc/storage/loop.h
@@ -6,21 +6,23 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "compiler.h"
+
 struct lxc_storage;
 
 struct bdev_specs;
 
 struct lxc_conf;
 
-extern int loop_clonepaths(struct lxc_storage *orig, struct lxc_storage *new,
-			   const char *oldname, const char *cname,
-			   const char *oldpath, const char *lxcpath, int snap,
-			   uint64_t newsize, struct lxc_conf *conf);
-extern int loop_create(struct lxc_storage *bdev, const char *dest,
-		       const char *n, struct bdev_specs *specs, const struct lxc_conf *conf);
-extern int loop_destroy(struct lxc_storage *orig);
-extern bool loop_detect(const char *path);
-extern int loop_mount(struct lxc_storage *bdev);
-extern int loop_umount(struct lxc_storage *bdev);
+__hidden extern int loop_clonepaths(struct lxc_storage *orig, struct lxc_storage *new,
+				    const char *oldname, const char *cname, const char *oldpath,
+				    const char *lxcpath, int snap, uint64_t newsize,
+				    struct lxc_conf *conf);
+__hidden extern int loop_create(struct lxc_storage *bdev, const char *dest, const char *n,
+				struct bdev_specs *specs, const struct lxc_conf *conf);
+__hidden extern int loop_destroy(struct lxc_storage *orig);
+__hidden extern bool loop_detect(const char *path);
+__hidden extern int loop_mount(struct lxc_storage *bdev);
+__hidden extern int loop_umount(struct lxc_storage *bdev);
 
 #endif /* __LXC_LOOP_H */
--- src/lxc/storage/nbd.h
+++ src/lxc/storage/nbd.h
@@ -6,25 +6,26 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "compiler.h"
+
 struct lxc_storage;
 
 struct bdev_specs;
 
 struct lxc_conf;
 
-extern int nbd_clonepaths(struct lxc_storage *orig, struct lxc_storage *new,
-			  const char *oldname, const char *cname,
-			  const char *oldpath, const char *lxcpath, int snap,
-			  uint64_t newsize, struct lxc_conf *conf);
-extern int nbd_create(struct lxc_storage *bdev, const char *dest, const char *n,
-		      struct bdev_specs *specs, const struct lxc_conf *conf);
-extern int nbd_destroy(struct lxc_storage *orig);
-extern bool nbd_detect(const char *path);
-extern int nbd_mount(struct lxc_storage *bdev);
-extern int nbd_umount(struct lxc_storage *bdev);
-
-extern bool attach_nbd(char *src, struct lxc_conf *conf);
-extern void detach_nbd_idx(int idx);
-extern bool requires_nbd(const char *path);
+__hidden extern int nbd_clonepaths(struct lxc_storage *orig, struct lxc_storage *new,
+				   const char *oldname, const char *cname, const char *oldpath,
+				   const char *lxcpath, int snap, uint64_t newsize,
+				   struct lxc_conf *conf);
+__hidden extern int nbd_create(struct lxc_storage *bdev, const char *dest, const char *n,
+			       struct bdev_specs *specs, const struct lxc_conf *conf);
+__hidden extern int nbd_destroy(struct lxc_storage *orig);
+__hidden extern bool nbd_detect(const char *path);
+__hidden extern int nbd_mount(struct lxc_storage *bdev);
+__hidden extern int nbd_umount(struct lxc_storage *bdev);
+__hidden extern bool attach_nbd(char *src, struct lxc_conf *conf);
+__hidden extern void detach_nbd_idx(int idx);
+__hidden extern bool requires_nbd(const char *path);
 
 #endif /* __LXC_NBD_H */
