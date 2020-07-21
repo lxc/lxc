04c5582a5943220e0acb359f2ecb2bc8e1c464a6
Christian Brauner <christian.brauner@ubuntu.com>
2020-07-21 13:57:16 +0200

    storage: hide unnecessary symbols
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/storage/overlay.h
+++ src/lxc/storage/overlay.h
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+#include "compiler.h"
 #include "storage.h"
 
 struct lxc_storage;
@@ -20,16 +21,16 @@ struct lxc_conf;
 
 struct lxc_rootfs;
 
-extern int ovl_clonepaths(struct lxc_storage *orig, struct lxc_storage *new,
-			  const char *oldname, const char *cname,
-			  const char *oldpath, const char *lxcpath, int snap,
-			  uint64_t newsize, struct lxc_conf *conf);
-extern int ovl_create(struct lxc_storage *bdev, const char *dest, const char *n,
-		      struct bdev_specs *specs, const struct lxc_conf *conf);
-extern int ovl_destroy(struct lxc_storage *orig);
-extern bool ovl_detect(const char *path);
-extern int ovl_mount(struct lxc_storage *bdev);
-extern int ovl_umount(struct lxc_storage *bdev);
+__hidden extern int ovl_clonepaths(struct lxc_storage *orig, struct lxc_storage *new,
+				   const char *oldname, const char *cname, const char *oldpath,
+				   const char *lxcpath, int snap, uint64_t newsize,
+				   struct lxc_conf *conf);
+__hidden extern int ovl_create(struct lxc_storage *bdev, const char *dest, const char *n,
+			       struct bdev_specs *specs, const struct lxc_conf *conf);
+__hidden extern int ovl_destroy(struct lxc_storage *orig);
+__hidden extern bool ovl_detect(const char *path);
+__hidden extern int ovl_mount(struct lxc_storage *bdev);
+__hidden extern int ovl_umount(struct lxc_storage *bdev);
 
 /* To be called from lxcapi_clone() in lxccontainer.c: When we clone a container
  * with overlay lxc.mount.entry entries we need to update absolute paths for
@@ -38,24 +39,23 @@ extern int ovl_umount(struct lxc_storage *bdev);
  * independent of each other since lxc_conf->mountlist may container more mount
  * entries (e.g. from other included files) than lxc_conf->unexpanded_config .
  */
-extern int ovl_update_abs_paths(struct lxc_conf *lxc_conf, const char *lxc_path,
-				const char *lxc_name, const char *newpath,
-				const char *newname);
+__hidden extern int ovl_update_abs_paths(struct lxc_conf *lxc_conf, const char *lxc_path,
+					 const char *lxc_name, const char *newpath,
+					 const char *newname);
 
 /* To be called from functions in lxccontainer.c: Get lower directory for
  * overlay rootfs.
  */
-extern const char *ovl_get_lower(const char *rootfs_path);
+__hidden extern const char *ovl_get_lower(const char *rootfs_path);
 
 /* Get rootfs path for overlay backed containers. Allocated memory must be freed
  * by caller.
  */
-extern char *ovl_get_rootfs(const char *rootfs_path, size_t *rootfslen);
+__hidden extern char *ovl_get_rootfs(const char *rootfs_path, size_t *rootfslen);
 
 /* Create upper- and workdirs for overlay mounts.
  */
-extern int ovl_mkdir(const struct mntent *mntent,
-		     const struct lxc_rootfs *rootfs, const char *lxc_name,
-		     const char *lxc_path);
+__hidden extern int ovl_mkdir(const struct mntent *mntent, const struct lxc_rootfs *rootfs,
+			      const char *lxc_name, const char *lxc_path);
 
 #endif /* __LXC_OVERLAY_H */
--- src/lxc/storage/storage_utils.h
+++ src/lxc/storage/storage_utils.h
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "compiler.h"
 #include "conf.h"
 #include "macro.h"
 
@@ -22,20 +23,19 @@
 struct lxc_storage;
 struct lxc_conf;
 
-extern bool attach_block_device(struct lxc_conf *conf);
-extern void detach_block_device(struct lxc_conf *conf);
-extern int blk_getsize(struct lxc_storage *bdev, uint64_t *size);
-extern int detect_fs(struct lxc_storage *bdev, char *type, int len);
-extern int do_mkfs_exec_wrapper(void *args);
-extern int is_blktype(struct lxc_storage *b);
-extern int mount_unknown_fs(const char *rootfs, const char *target,
-			    const char *options);
-extern int find_fstype_cb(char *buffer, void *data);
-extern const char *linkderef(const char *path, char *dest);
-extern bool unpriv_snap_allowed(struct lxc_storage *b, const char *t, bool snap,
-				bool maybesnap);
-extern uint64_t get_fssize(char *s);
-extern bool is_valid_storage_type(const char *type);
-extern int storage_destroy_wrapper(void *data);
+__hidden extern bool attach_block_device(struct lxc_conf *conf);
+__hidden extern void detach_block_device(struct lxc_conf *conf);
+__hidden extern int blk_getsize(struct lxc_storage *bdev, uint64_t *size);
+__hidden extern int detect_fs(struct lxc_storage *bdev, char *type, int len);
+__hidden extern int do_mkfs_exec_wrapper(void *args);
+__hidden extern int is_blktype(struct lxc_storage *b);
+__hidden extern int mount_unknown_fs(const char *rootfs, const char *target, const char *options);
+__hidden extern int find_fstype_cb(char *buffer, void *data);
+__hidden extern const char *linkderef(const char *path, char *dest);
+__hidden extern bool unpriv_snap_allowed(struct lxc_storage *b, const char *t, bool snap,
+					 bool maybesnap);
+__hidden extern uint64_t get_fssize(char *s);
+__hidden extern bool is_valid_storage_type(const char *type);
+__hidden extern int storage_destroy_wrapper(void *data);
 
 #endif /* __LXC_STORAGE_UTILS_H */
