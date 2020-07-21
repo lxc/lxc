297b04e2823a76fa1b8d3cb660e3ed5297bbf943
Christian Brauner <christian.brauner@ubuntu.com>
2020-07-21 13:19:03 +0200

    uuid: hide unnecessary symbols
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/uuid.h
+++ src/lxc/uuid.h
@@ -9,16 +9,18 @@
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
 
+#include "compiler.h"
+
 typedef union lxc_id128 lxc_id128_t;
 
 union lxc_id128 {
-        uint8_t bytes[16];
-        uint64_t qwords[2];
-}
-;
-extern int lxc_id128_randomize(lxc_id128_t *ret);
-extern int lxc_id128_write(const char *p, lxc_id128_t id);
-extern int lxc_id128_write_fd(int fd, lxc_id128_t id);
-extern char *id128_to_uuid_string(lxc_id128_t id, char s[37]);
+	uint8_t bytes[16];
+	uint64_t qwords[2];
+};
+
+__hidden extern int lxc_id128_randomize(lxc_id128_t *ret);
+__hidden extern int lxc_id128_write(const char *p, lxc_id128_t id);
+__hidden extern int lxc_id128_write_fd(int fd, lxc_id128_t id);
+__hidden extern char *id128_to_uuid_string(lxc_id128_t id, char s[37]);
 
 #endif /* __LXC_UUID_H */
