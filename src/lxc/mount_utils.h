14df702190f8bb7693c65a36392e09a1dd66e96b
Christian Brauner <christian.brauner@ubuntu.com>
2020-07-22 11:45:15 +0200

    mount_utils: add mount_filesystem() helper
    that translates between the two mount apis.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/mount_utils.h
+++ src/lxc/mount_utils.h
@@ -148,4 +148,6 @@ __hidden extern int mnt_attributes_new(unsigned int old_flags, unsigned int *new
 
 __hidden extern int mnt_attributes_old(unsigned int new_flags, unsigned int *old_flags);
 
+__hidden extern int mount_filesystem(const char *fs_name, const char *path, unsigned int attr_flags);
+
 #endif /* __LXC_MOUNT_UTILS_H */
