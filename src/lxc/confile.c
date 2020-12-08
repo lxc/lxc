0ab4c703bd91e29fd2262db602a4a6133cd3b3e7
Christian Brauner <christian.brauner@ubuntu.com>
2020-12-08 18:54:34 +0100

    confile: cleanup clr_config_net_veth_pair()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -5326,7 +5326,7 @@ static int clr_config_net_veth_pair(const char *key, struct lxc_conf *lxc_conf,
 	struct lxc_netdev *netdev = data;
 
 	if (!netdev)
-		return -1;
+		return ret_errno(EINVAL);
 
 	netdev->priv.veth_attr.pair[0] = '\0';
 
