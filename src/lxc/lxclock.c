feb5f3b1856992dd15b77106247c47121566e2fe
Christian Brauner <christian.brauner@ubuntu.com>
2020-12-09 11:33:38 +0100

    lxclock: cleanup dump_stacktrace()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/lxclock.c
+++ src/lxc/lxclock.c
@@ -36,7 +36,6 @@ static inline void dump_stacktrace(void)
 	void *array[MAX_STACKDEPTH];
 	size_t size;
 	char **strings;
-	size_t i;
 
 	size = backtrace(array, MAX_STACKDEPTH);
 	strings = backtrace_symbols(array, size);
@@ -44,7 +43,7 @@ static inline void dump_stacktrace(void)
 	/* Using fprintf here as our logging module is not thread safe. */
 	fprintf(stderr, "\tObtained %zu stack frames\n", size);
 
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 		fprintf(stderr, "\t\t%s\n", strings[i]);
 
 	free(strings);
