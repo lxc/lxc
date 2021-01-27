e8a2d23cc7b3ad0f360be5c1f91e570755b06024
Christian Brauner <christian.brauner@ubuntu.com>
2021-01-27 20:48:41 +0100

    attach: mark do_attach() as __noreturn
    Cc: stable-4.0
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/attach.c
+++ src/lxc/attach.c
@@ -644,7 +644,7 @@ static void lxc_put_attach_clone_payload(struct attach_clone_payload *p)
 	}
 }
 
-static int do_attach(struct attach_clone_payload *payload)
+__noreturn static int do_attach(struct attach_clone_payload *payload)
 {
 	int lsm_fd, ret;
 	uid_t new_uid;
