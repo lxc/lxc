02c90439e65a1627e925cfdcda29ba898cca8d97
Christian Brauner <christian.brauner@ubuntu.com>
2021-01-27 20:42:21 +0100

    attach: use free_disarm()
    Cc: stable-4.0
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/attach.c
+++ src/lxc/attach.c
@@ -109,8 +109,7 @@ static inline void lxc_proc_close_ns_fd(struct lxc_proc_context_info *ctx)
 
 static void lxc_proc_put_context_info(struct lxc_proc_context_info *ctx)
 {
-	free(ctx->lsm_label);
-	ctx->lsm_label = NULL;
+	free_disarm(ctx->lsm_label);
 
 	if (ctx->container) {
 		lxc_container_put(ctx->container);
