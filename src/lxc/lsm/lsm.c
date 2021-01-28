19615bfcf71cfd0ccedb7dadefb5e83915d13fa0
Christian Brauner <christian.brauner@ubuntu.com>
2021-01-28 19:19:32 +0100

    lsm: s/lsm_init/lsm_init_static/g
    Make it clear that there is nothing to free here.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/lsm/lsm.c
+++ src/lxc/lsm/lsm.c
@@ -21,7 +21,7 @@ __hidden extern struct lsm_ops *lsm_apparmor_ops_init(void);
 __hidden extern struct lsm_ops *lsm_selinux_ops_init(void);
 __hidden extern struct lsm_ops *lsm_nop_ops_init(void);
 
-struct lsm_ops *lsm_init(void)
+struct lsm_ops *lsm_init_static(void)
 {
 	struct lsm_ops *ops = NULL;
 
