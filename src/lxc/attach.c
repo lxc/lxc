971dcc4313b9eb8892fc46b23c7a18474fb6407e
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-01 14:44:45 +0100

    attach: log failues to dup2() with SYSDEBUG()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/attach.c
+++ src/lxc/attach.c
@@ -828,15 +828,15 @@ __noreturn static void do_attach(struct attach_clone_payload *payload)
 	 */
 	if (options->stdin_fd >= 0 && options->stdin_fd != STDIN_FILENO)
 		if (dup2(options->stdin_fd, STDIN_FILENO))
-			DEBUG("Failed to replace stdin with %d", options->stdin_fd);
+			SYSDEBUG("Failed to replace stdin with %d", options->stdin_fd);
 
 	if (options->stdout_fd >= 0 && options->stdout_fd != STDOUT_FILENO)
 		if (dup2(options->stdout_fd, STDOUT_FILENO))
-			DEBUG("Failed to replace stdout with %d", options->stdin_fd);
+			SYSDEBUG("Failed to replace stdout with %d", options->stdin_fd);
 
 	if (options->stderr_fd >= 0 && options->stderr_fd != STDERR_FILENO)
 		if (dup2(options->stderr_fd, STDERR_FILENO))
-			DEBUG("Failed to replace stderr with %d", options->stdin_fd);
+			SYSDEBUG("Failed to replace stderr with %d", options->stdin_fd);
 
 	/* close the old fds */
 	if (options->stdin_fd > STDERR_FILENO)
