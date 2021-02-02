db4482effe8b4868b494ff8e6eb73e79195b6ee7
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-02 18:08:14 +0100

    cgroups: move functions after methods
    This makes it more obvious that they are separate.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/cgroups/cgfsng.c
+++ src/lxc/cgroups/cgfsng.c
@@ -2500,31 +2500,6 @@ __cgfsng_ops static bool cgfsng_attach(struct cgroup_ops *ops,
 	return true;
 }
 
-int cgroup_get(struct lxc_conf *conf,
-	       const char *name, const char *lxcpath,
-	       const char *filename, char *buf, size_t len)
-{
-	__do_close int unified_fd = -EBADF;
-	ssize_t ret;
-
-	if (!conf || is_empty_string(filename) || is_empty_string(name) ||
-	    is_empty_string(lxcpath))
-		return ret_errno(EINVAL);
-
-	if ((buf && !len) || (len && !buf))
-		return ret_errno(EINVAL);
-
-	unified_fd = lxc_cmd_get_cgroup2_fd(name, lxcpath);
-	if (unified_fd < 0)
-		return ret_errno(ENOCGROUP2);
-
-	ret = lxc_read_try_buf_at(unified_fd, filename, buf, len);
-	if (ret < 0)
-		SYSERROR("Failed to read cgroup value");
-
-	return ret;
-}
-
 /* Called externally (i.e. from 'lxc-cgroup') to query cgroup limits.  Here we
  * don't have a cgroup_data set up, so we ask the running container through the
  * commands API for the cgroup path.
@@ -2673,35 +2648,6 @@ static int device_cgroup_rule_parse(struct device_item *device, const char *key,
 		return -1;
 
 	return device_cgroup_parse_access(device, ++val);
-}
-
-int cgroup_set(struct lxc_conf *conf, const char *name, const char *lxcpath,
-	       const char *filename, const char *value)
-{
-	__do_close int unified_fd = -EBADF;
-	ssize_t ret;
-
-	if (!conf || is_empty_string(filename) || is_empty_string(value) ||
-	    is_empty_string(name) || is_empty_string(lxcpath))
-		return ret_errno(EINVAL);
-
-	unified_fd = lxc_cmd_get_cgroup2_fd(name, lxcpath);
-	if (unified_fd < 0)
-		return ret_errno(ENOCGROUP2);
-
-	if (strncmp(filename, "devices.", STRLITERALLEN("devices.")) == 0) {
-		struct device_item device = {};
-
-		ret = device_cgroup_rule_parse(&device, filename, value);
-		if (ret < 0)
-			return log_error_errno(-1, EINVAL, "Failed to parse device string %s=%s", filename, value);
-
-		ret = lxc_cmd_add_bpf_device_cgroup(name, lxcpath, &device);
-	} else {
-		ret = lxc_writeat(unified_fd, filename, value, strlen(value));
-	}
-
-	return ret;
 }
 
 /* Called externally (i.e. from 'lxc-cgroup') to set new cgroup limits.  Here we
@@ -3553,4 +3499,58 @@ struct cgroup_ops *cgfsng_ops_init(struct lxc_conf *conf)
 	cgfsng_ops->get_limiting_cgroup			= cgfsng_get_limiting_cgroup;
 
 	return move_ptr(cgfsng_ops);
+}
+
+int cgroup_get(struct lxc_conf *conf,
+	       const char *name, const char *lxcpath,
+	       const char *filename, char *buf, size_t len)
+{
+	__do_close int unified_fd = -EBADF;
+	ssize_t ret;
+
+	if (!conf || is_empty_string(filename) || is_empty_string(name) ||
+	    is_empty_string(lxcpath))
+		return ret_errno(EINVAL);
+
+	if ((buf && !len) || (len && !buf))
+		return ret_errno(EINVAL);
+
+	unified_fd = lxc_cmd_get_cgroup2_fd(name, lxcpath);
+	if (unified_fd < 0)
+		return ret_errno(ENOCGROUP2);
+
+	ret = lxc_read_try_buf_at(unified_fd, filename, buf, len);
+	if (ret < 0)
+		SYSERROR("Failed to read cgroup value");
+
+	return ret;
+}
+
+int cgroup_set(struct lxc_conf *conf, const char *name, const char *lxcpath,
+	       const char *filename, const char *value)
+{
+	__do_close int unified_fd = -EBADF;
+	ssize_t ret;
+
+	if (!conf || is_empty_string(filename) || is_empty_string(value) ||
+	    is_empty_string(name) || is_empty_string(lxcpath))
+		return ret_errno(EINVAL);
+
+	unified_fd = lxc_cmd_get_cgroup2_fd(name, lxcpath);
+	if (unified_fd < 0)
+		return ret_errno(ENOCGROUP2);
+
+	if (strncmp(filename, "devices.", STRLITERALLEN("devices.")) == 0) {
+		struct device_item device = {};
+
+		ret = device_cgroup_rule_parse(&device, filename, value);
+		if (ret < 0)
+			return log_error_errno(-1, EINVAL, "Failed to parse device string %s=%s", filename, value);
+
+		ret = lxc_cmd_add_bpf_device_cgroup(name, lxcpath, &device);
+	} else {
+		ret = lxc_writeat(unified_fd, filename, value, strlen(value));
+	}
+
+	return ret;
 }
