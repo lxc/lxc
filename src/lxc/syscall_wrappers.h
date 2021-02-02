70e73e224b5919496c90d039c07d12b8ec1f6c16
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-02 13:44:37 +0100

    syscall_wrappers: add PROTECT_OPEN_W_* variants
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/syscall_wrappers.h
+++ src/lxc/syscall_wrappers.h
@@ -268,6 +268,9 @@ struct lxc_open_how {
 #define PROTECT_OPEN_WITH_TRAILING_SYMLINKS (O_CLOEXEC | O_NOCTTY | O_RDONLY)
 #define PROTECT_OPEN (PROTECT_OPEN_WITH_TRAILING_SYMLINKS | O_NOFOLLOW)
 
+#define PROTECT_OPEN_W_WITH_TRAILING_SYMLINKS (O_CLOEXEC | O_NOCTTY | O_WRONLY)
+#define PROTECT_OPEN_W (PROTECT_OPEN_WITH_TRAILING_SYMLINKS | O_NOFOLLOW)
+
 #ifndef HAVE_OPENAT2
 static inline int openat2(int dfd, const char *filename, struct lxc_open_how *how, size_t size)
 {
