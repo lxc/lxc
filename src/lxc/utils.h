f0aa6dd65b5f0e1c07fe3d3bd3ef54935ed28e2b
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-05 12:07:44 +0100

    utils: rework lxc_setgroups()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/utils.h
+++ src/lxc/utils.h
@@ -156,7 +156,7 @@ __hidden extern bool task_blocks_signal(pid_t pid, int signal);
  * If LXC_INVALID_{G,U}ID is passed then the set{g,u}id() will not be called.
  */
 __hidden extern bool lxc_switch_uid_gid(uid_t uid, gid_t gid);
-__hidden extern bool lxc_setgroups(int size, gid_t list[]);
+__hidden extern bool lxc_setgroups(gid_t list[], size_t size);
 __hidden extern bool lxc_drop_groups(void);
 
 /* Find an unused loop device and associate it with source. */
