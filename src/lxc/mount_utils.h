ca9055b47399a2040a5e1c2a8da591832380b168
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-05 17:34:57 +0100

    mount_utils: kill mount_at()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/mount_utils.h
+++ src/lxc/mount_utils.h
@@ -152,11 +152,6 @@ __hidden extern int mnt_attributes_old(unsigned int new_flags, unsigned int *old
 
 __hidden extern int mount_filesystem(const char *fs_name, const char *path, unsigned int attr_flags);
 
-__hidden extern int mount_at(int dfd, const char *src_under_dfd,
-			     const char *dst_under_dfd, __u64 o_flags,
-			     __u64 resolve_flags, const char *fstype,
-			     unsigned int mnt_flags, const void *data);
-
 __hidden extern int mount_from_at(int dfd_from, const char *path_from,
 				  __u64 o_flags_from, __u64 resolve_flags_from,
 				  int dfd_to, const char *path_to,
