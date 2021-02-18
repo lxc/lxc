0a150695b49c95e3fa3c9a787e235cfc5bfdd0b3
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-18 14:50:03 +0100

    bpf: rework live device cgroup update
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/cgroups/cgroup2_devices.h
+++ src/lxc/cgroups/cgroup2_devices.h
@@ -100,6 +100,9 @@ __hidden extern int bpf_list_add_device(struct lxc_list *devices,
 					struct device_item *device);
 __hidden extern bool bpf_cgroup_devices_attach(struct cgroup_ops *ops,
 					       struct lxc_list *devices);
+__hidden extern bool bpf_cgroup_devices_update(struct cgroup_ops *ops,
+					       struct device_item *new,
+					       struct lxc_list *devices);
 
 define_cleanup_function(struct bpf_program *, bpf_program_free);
 #define __do_bpf_program_free call_cleaner(bpf_program_free)
