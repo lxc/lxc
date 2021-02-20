75f08c85823ed8534b486bc6dd5c7e864c29f655
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-20 01:30:33 +0100

    macro: add pointer error encoding support
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/macro.h
+++ src/lxc/macro.h
@@ -21,6 +21,8 @@
 #include <sys/un.h>
 #include <unistd.h>
 
+#include "compiler.h"
+
 #ifndef PATH_MAX
 #define PATH_MAX 4096
 #endif
@@ -669,5 +671,42 @@ enum {
 		(a) = (b);             \
 		(b) = __tmp;           \
 	} while (0)
+
+#define MAX_ERRNO 4095
+
+#define IS_ERR_VALUE(x) unlikely((x) >= (unsigned long)-MAX_ERRNO)
+
+static inline void *ERR_PTR(long error)
+{
+	return (void *)error;
+}
+
+static inline long PTR_ERR(const void *ptr)
+{
+	return (long)ptr;
+}
+
+static inline long IS_ERR(const void *ptr)
+{
+	return IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline long IS_ERR_OR_NULL(const void *ptr)
+{
+	return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline void *ERR_CAST(const void *ptr)
+{
+	return (void *)ptr;
+}
+
+static inline int PTR_RET(const void *ptr)
+{
+	if (IS_ERR(ptr))
+		return PTR_ERR(ptr);
+	else
+		return 0;
+}
 
 #endif /* __LXC_MACRO_H */
