aa659ba5478d6982088701cf08cc65b6fb56f6cc
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-23 13:58:13 +0100

    log: add syswarn_set()
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/log.h
+++ src/lxc/log.h
@@ -530,6 +530,14 @@ __lxc_unused static inline void LXC_##LEVEL(struct lxc_log_locinfo* locinfo,	\
 		__internal_ret__;                             \
 	})
 
+#define syswarn_set(__ret__, format, ...)                     \
+	({                                                    \
+		typeof(__ret__) __internal_ret__ = (__ret__); \
+		errno = abs(__ret__);                         \
+		SYSWARN(format, ##__VA_ARGS__);               \
+		__internal_ret__;                             \
+	})
+
 #define log_error(__ret__, format, ...)                       \
 	({                                                    \
 		typeof(__ret__) __internal_ret__ = (__ret__); \
