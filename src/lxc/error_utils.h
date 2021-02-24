38fa7e479cea516d2cf303bbe8f3a8f88ea2a3c9
Christian Brauner <christian.brauner@ubuntu.com>
2021-02-24 09:16:09 +0100

    error_utils: move error helper to separate header
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

---
+++ src/lxc/error_utils.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: LGPL-2.1+ */
+
+#ifndef __LXC_ERROR_UTILS_H
+#define __LXC_ERROR_UTILS_H
+
+#define MAX_ERRNO 4095
+
+#define IS_ERR_VALUE(x) unlikely((x) >= (unsigned long)-MAX_ERRNO)
+
+static inline void *ERR_PTR(long error)
+{
+	return (void *)error;
+}
+
+static inline long PTR_ERR(const void *ptr)
+{
+	return (long)ptr;
+}
+
+static inline long IS_ERR(const void *ptr)
+{
+	return IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline long IS_ERR_OR_NULL(const void *ptr)
+{
+	return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline void *ERR_CAST(const void *ptr)
+{
+	return (void *)ptr;
+}
+
+static inline int PTR_RET(const void *ptr)
+{
+	if (IS_ERR(ptr))
+		return PTR_ERR(ptr);
+	else
+		return 0;
+}
+
+#endif /* __LXC_ERROR_UTILS_H */
