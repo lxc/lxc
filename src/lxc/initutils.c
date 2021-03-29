63340988415dacdf42969ac2d518b5c8e158d9dd
Christian Brauner <christian.brauner@ubuntu.com>
2021-03-29 12:49:51 +0200

    conf: fix thread_local support detection
    Our detection for TLS wasn't working. Fix it.
    
    Fixes: https://github.com/lxc/lxd/issues/8327
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/initutils.c
+++ src/lxc/initutils.c
@@ -54,8 +54,10 @@ const char *lxc_global_config_value(const char *option_name)
 	};
 
 	/* placed in the thread local storage pool for non-bionic targets */
-#ifdef HAVE_TLS
+#if defined(THREAD_LOCAL_STORAGE_SUPPORTED)
 	static thread_local const char *values[sizeof(options) / sizeof(options[0])] = {0};
+#elif defined(ENFORCE_THREAD_SAFETY)
+	#error ENFORCE_THREAD_SAFETY was set but cannot be guaranteed due to missing TLS
 #else
 	static const char *values[sizeof(options) / sizeof(options[0])] = {0};
 #endif
