535bc5aac240b7acce357dcb9e83c25172e8eeee
Christian Brauner <christian.brauner@ubuntu.com>
2021-04-22 09:44:07 +0200

    strchrnul: include header
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/include/strchrnul.c
+++ src/include/strchrnul.c
@@ -23,6 +23,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "strchrnul.h"
+
 /* Find the first occurrence of C in S or the final NUL byte.  */
 char *strchrnul(const char *s, int c_in)
 {
