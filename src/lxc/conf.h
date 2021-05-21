493ae3fe7e660975d8083db4e423e551276de7f8
Christian Brauner <christian.brauner@ubuntu.com>
2021-05-21 10:06:27 +0200

    conf: move file descriptor synchronization with child into single function
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/conf.h
+++ src/lxc/conf.h
@@ -582,7 +582,7 @@ static inline int chown_mapped_root(const char *path, const struct lxc_conf *con
 	return userns_exec_mapped_root(path, -EBADF, conf);
 }
 
-__hidden int lxc_setup_devpts_parent(struct lxc_handler *handler);
+__hidden extern int lxc_sync_fds_parent(struct lxc_handler *handler);
 
 static inline const char *get_rootfs_mnt(const struct lxc_rootfs *rootfs)
 {
