lxc/lxc

commit f382bcc6d820c46a54ec928ea7cbfaa3847adc66

terminal: log TIOCGPTPEER failure less alarmingly
This is not a fatal error and the fallback codepath is equally safe.
When we use TIOCGPTPEER we're using a stashed fd to the container's
devpts mount's ptmx device and allocating a new fd non-path based
through this ioctl. If this ioctl can't be used we're falling back to
allocating a pts device from the host's devpts mount's ptmx device which
is path-based but is not under control of the container and so that's
safe. The difference is just that the first method gets you a nice
native terminal with all the pleasantries of having tty and friends
working whereas the latter method does not.

Fixes: #3625
Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

src/lxc/terminal.c
@@ -931,7 +931,17 @@ static int lxc_terminal_create_native(const char *name, const char *lxcpath, str
 
 	terminal->pty = ioctl(terminal->ptx, TIOCGPTPEER, O_RDWR | O_NOCTTY | O_CLOEXEC);
 	if (terminal->pty < 0) {
-		SYSWARN("Failed to allocate new pty device");
+		switch (errno) {
+		case ENOTTY:
+			SYSTRACE("Pure fd-based terminal allocation not possible");
+			break;
+		case ENOSPC:
+			SYSTRACE("Exceeding number of allocatable terminals");
+			break;
+		default:
+			SYSWARN("Failed to allocate new pty device");
+			break;
+		}
 		goto err;
 	}
 

commit 7a3165591780609a4545979c6eb66d640b8a1fa5

terminal: fix error handling
Fixes: f382bcc6d820 ("terminal: log TIOCGPTPEER failure less alarmingly")
Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

src/lxc/terminal.c
@@ -920,8 +920,12 @@ static int lxc_terminal_create_native(const char *name, const char *lxcpath, str
 		return log_error_errno(-1, errno, "Failed to receive devpts fd");
 
 	terminal->ptx = open_beneath(devpts_fd, "ptmx", O_RDWR | O_NOCTTY | O_CLOEXEC);
-	if (terminal->ptx < 0)
-		return log_error_errno(-1, errno, "Failed to open terminal multiplexer device");
+	if (terminal->ptx < 0) {
+		if (errno == ENOSPC)
+			return systrace("Exceeded number of allocatable terminals");
+
+		return syserror("Failed to open terminal multiplexer device");
+	}
 
 	ret = unlockpt(terminal->ptx);
 	if (ret < 0) {
@@ -936,7 +940,7 @@ static int lxc_terminal_create_native(const char *name, const char *lxcpath, str
 			SYSTRACE("Pure fd-based terminal allocation not possible");
 			break;
 		case ENOSPC:
-			SYSTRACE("Exceeding number of allocatable terminals");
+			SYSTRACE("Exceeded number of allocatable terminals");
 			break;
 		default:
 			SYSWARN("Failed to allocate new pty device");