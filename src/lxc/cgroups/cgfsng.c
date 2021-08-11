38db8993803d0635a91687836a5c043bb8c876ed
Jim Ferrigno <jferrign@Jamess-MacBook-Pro.local>
2021-08-11 14:44:32 -0400

    cgroups: fix cpu bitmasks
    Link: https://discuss.linuxcontainers.org/t/lxc-4-0-9-lxc-start-sigabrt-on-systems-with-defined-offline-cpus-and-a-total-number-of-cpus-divisible-by-32
    Signed-off-by: Jim Ferrigno <jim.ferrigno@oracle.com>
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/lxc/cgroups/cgfsng.c
+++ src/lxc/cgroups/cgfsng.c
@@ -596,7 +596,6 @@ static bool cpuset1_cpus_initialize(int dfd_parent, int dfd_child,
 
 		if (maxposs < maxisol)
 			maxposs = maxisol;
-		maxposs++;
 	} else {
 		TRACE("The path \""__ISOL_CPUS"\" to read isolated cpus from does not exist");
 	}
@@ -615,7 +614,6 @@ static bool cpuset1_cpus_initialize(int dfd_parent, int dfd_child,
 
 		if (maxposs < maxoffline)
 			maxposs = maxoffline;
-		maxposs++;
 	} else {
 		TRACE("The path \""__OFFLINE_CPUS"\" to read offline cpus from does not exist");
 	}
